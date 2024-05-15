a98bf69e38b07a930793cffad5fdba3000404edc
KrisCenGH <115469642+KrisCenGH@users.noreply.github.com>
2024-05-15 18:04:06 +0800

    Update freenode.js

--- js/freenode.js
+++ js/freenode.js
@@ -3,8 +3,8 @@ fetch('https://api.github.com/repos/tolinkshare/freenode/contents/README.md')
     .then(data => {
         const markdownContent = atob(data.content); // 解码Base64编码的Markdown内容
         const extractedText = extractTextBetweenThirdAndFourthBackticks(markdownContent);
-        const encodedElement = encodeWithLineBreaks(extractedText); // 逐行进行base64编码
-        document.getElementById('content').innerText = btoa(extractedText); // 输出文本内容
+        const Text = removeFirstNewLine(extractedText);
+        document.getElementById('content').innerText = btoa(Text); // 输出文本内容
     })
     .catch(error => console.error('Error fetching README.md:', error));
 
@@ -21,21 +21,8 @@ fetch('https://api.github.com/repos/tolinkshare/freenode/contents/README.md')
         }
     }
 
-    function encodeWithLineBreaks(str) {
-        let result = '';
-        let buffer = '';
-
-        for (let i = 0; i < str.length; ++i) {
-            if (str[i] === '\n') {
-                const base64 = btoa(buffer);
-                result += base64 + '\n';
-                buffer = '';
-            } else {
-                buffer += str[i];
-            }
-        }
-        const base64 = btoa(buffer);
-        result += base64;
-
-        return result;
+    function removeFirstNewLine(str) {
+        let res = '';
+        res = str.substring(1);
+        return res;
     }
